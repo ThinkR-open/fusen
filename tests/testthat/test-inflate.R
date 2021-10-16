6e1b1a6c6df11cd67a3cc03c60874ac20607d20a
statnmap <sebastien@thinkr.fr>
2021-10-16 19:10:35 +0200

    Fix no roxygen skeleton
    - issue #35 #55
    - simplify chunk names. issue #40

--- tests/testthat/test-inflate.R
+++ tests/testthat/test-inflate.R
@@ -1,6 +1,5 @@
 # Create a new project
-tmpdir <- tempdir()
-dummypackage <- file.path(tmpdir, "dummypackage")
+dummypackage <- tempfile("dummypackage")
 dir.create(dummypackage)
 
 # {fusen} steps
@@ -10,13 +9,13 @@ dev_file <- add_dev_history(pkg = dummypackage, overwrite = TRUE, open = FALSE)
 usethis::with_project(dummypackage, {
 
   # More complicated example for tests
-    testfile <- "tests-templates/dev-template-tests.Rmd"
-    file.copy(
-      system.file(testfile, package = "fusen"),
-      dev_file,
-      overwrite = TRUE
-    )
-
+  testfile <- "tests-templates/dev-template-tests.Rmd"
+  file.copy(
+    system.file(testfile, package = "fusen"),
+    dev_file,
+    overwrite = TRUE
+  )
+# browser()
   inflate(pkg = dummypackage, rmd = dev_file, name = "exploration", check = FALSE)
 
   test_that("inflate() worked correctly", {
@@ -31,6 +30,18 @@ usethis::with_project(dummypackage, {
     expect_true(file.exists(my_sixth_median_file))
     myuppercasefunctionfile <- file.path(dummypackage, "R", "myuppercasefunction.R")
     expect_true(file.exists(myuppercasefunctionfile))
+
+    # Found with chunk named `fun`
+    my_noroxfunctionfile <- file.path(dummypackage, "R", "my_norox.R")
+    expect_true(file.exists(my_noroxfunctionfile))
+    # Found with chunk named `fun-norox2`
+    my_norox2functionfile <- file.path(dummypackage, "R", "my_norox2.R")
+    expect_true(file.exists(my_norox2functionfile))
+    # Found with chunk named `fun_space`
+    my_spacefunctionfile <- file.path(dummypackage, "R", "my_space.R")
+    expect_true(file.exists(my_spacefunctionfile))
+    my_space2functionfile <- file.path(dummypackage, "R", "my_space2.R")
+    expect_true(file.exists(my_space2functionfile))
 
     # examples in R files
     my_median_lines <- readLines(my_median_file)
@@ -50,9 +61,35 @@ usethis::with_project(dummypackage, {
     expect_true(all(my_sixth_median_lines[9:11] == c(
       "#' @examples", "#' my.sixth.median_function(1:12)", "#' my.sixth.median_function(8:20)"
     )))
+    # _no roxygen at all
+    my_norox_lines <- readLines(my_noroxfunctionfile)
+    expect_true(all(my_norox_lines == c(
+      "#' @noRd", "my_norox <- function(x) {", "  x + 10", "}"
+    )))
+    # _no roxygen but examples
+    my_norox2_lines <- readLines(my_norox2functionfile)
+    expect_true(all(my_norox2_lines == c(
+      "#' @export", "#' @examples",
+      "#' my_norox2(10)", "my_norox2 <- function(x) {", "  x + 10", "}"
+    )))
+    # _extra empty line and examples
+    my_space_lines <- readLines(my_spacefunctionfile)
+    expect_true(all(my_space_lines[6:10] == c(
+      "#' @examples", "#' my_space(10)", "#' @export", "",  "my_space <- function(x) {"
+    )))
+    # _extra empty line and noRd
+    my_space2_lines <- readLines(my_space2functionfile)
+    expect_true(all(my_space2_lines[6:8] == c(
+      "#' @noRd", "",  "my_space2 <- function(x) {"
+    )))
 
     # vignette
-    expect_true(file.exists(file.path(dummypackage, "vignettes", "exploration.Rmd")))
+    the_vignette <- file.path(dummypackage, "vignettes", "exploration.Rmd")
+    expect_true(file.exists(the_vignette))
+    vignette_lines <- readLines(the_vignette)
+    # No dev chunks in the vignette
+    expect_false(any(grepl("```{r dev}", vignette_lines, fixed = TRUE)))
+    expect_false(any(grepl("```{r development-1", vignette_lines, fixed = TRUE)))
 
     # tests
     expect_true(file.exists(
@@ -75,7 +112,7 @@ usethis::with_project(dummypackage, {
 
 # Test package no check errors ----
 usethis::with_project(dummypackage, {
-
+# browser()
   test_that("inflate() output error", {
     # Do not check inside check if on CRAN
     skip_on_os(os = c("windows", "solaris"))
@@ -108,7 +145,7 @@ usethis::with_project(dummypackage, {
 
 # Test no problem with special character in YAML ----
 if (packageVersion("parsermd") > "0.1.2") {
-  dummypackage.special <- file.path(tmpdir, "dummypackage.special")
+  dummypackage.special <- tempfile("dummypackage.special")
   dir.create(dummypackage.special)
 
   # {fusen} steps
@@ -228,6 +265,7 @@ usethis::with_project(dummypackage, {
   unlink(file.path(dummypackage, "vignettes"), recursive = TRUE)
   unlink(file.path(dummypackage, "tests"), recursive = TRUE)
 
+  # browser()
   # Tests errors - duplicate chunk names
   file.copy(
     system.file("tests-templates/dev-template-stop-duplicate-label.Rmd", package = "fusen"),
@@ -324,8 +362,7 @@ unlink(dummypackage, recursive = TRUE)
 
 
 # Test stop when no DESCRIPTION file ----
-tmpdir <- tempdir()
-dummypackage <- file.path(tmpdir, "descpackage")
+dummypackage <- tempfile("descpackage")
 dir.create(dummypackage)
 dev_file <- add_dev_history(pkg = dummypackage, overwrite = TRUE, open = FALSE)
 
@@ -341,4 +378,34 @@ if (exists("dummypackage.special")) {
   unlink(dummypackage.special, recursive = TRUE)
 }
 
+# Deal with noRd, examples and dontrun ----
+dummypackage <- tempfile("nordpackage")
+dir.create(dummypackage)
+dev_file <- add_dev_history(pkg = dummypackage, overwrite = TRUE, open = FALSE)
+
+usethis::with_project(dummypackage, {
+  file.copy(
+    system.file("tests-templates/dev-template-nord-but-example.Rmd", package = "fusen"),
+    dev_file,
+    overwrite = TRUE
+  )
+
+  test_that("Deal with noRd but examples", {
+    # No error
+    expect_error(inflate(pkg = dummypackage, rmd = dev_file, check = FALSE), regexp = NA)
+    # Check error
+    # TRY the check
+  })
+})
+
+# Delete dummy package
+unlink(dummypackage, recursive = TRUE)
+
 # Do not create a second package with {fusen} in the same session, as it will mess up with `setwd()` and {usethis} needs these `setwd()`...
+
+#' stop() if @noRd but there is an example...
+#' Or suggests \dontrun{}, but need to be taken into account in vignette
+# E> Quitting from lines 89-90 (exploration.Rmd)
+# E> Error: processing vignette 'exploration.Rmd' failed with diagnostics:
+#   E> impossible de trouver la fonction "my_norox2"
+# E> --- failed re-building ‘exploration.Rmd’
